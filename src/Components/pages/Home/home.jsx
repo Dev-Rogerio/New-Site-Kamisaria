efecfaeaafbe478ca45fe259f8aa4f3f48850f25
Rogerio Almeida <roger.ngt3494@gmail.com>
2025-06-04 14:43:59 -0300

    Corrige nome do arquivo para capaHome.jpg com H maiúsculo

--- src/Components/pages/Home/home.jsx
+++ src/Components/pages/Home/home.jsx
@@ -5,7 +5,7 @@ import Nav from "../../common/nav/Nav";
 import "./home.css";
 import "../Sales/sales.css";
 
-import Capa from "../../Img/temp.jpg";
+import Capa from "../../Img/capaHome.jpg";
 import One from "../../Img/kz1.jpg";
 import Two from "../../Img/kz2.jpg";
 import Three from "../../Img/kz3.jpg";
